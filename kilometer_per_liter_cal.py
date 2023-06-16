6372e433390ed0efe1e790a7d33920ca5b64f54d
Sepgamer <126633525+Sepgamer@users.noreply.github.com>
2023-06-16 12:48:06 +0330

    Add files via upload
    This module is applicable for calculating KPL(kilometer_per_liter)

---
+++ kilometer_per_liter_cal.py
@@ -0,0 +1,20 @@
+"""
+    *   Developer   : Sepehr Hosseini
+    *   Email       : Sepgamer87@Gmail.Com
+    *   Description : # This program show car's Kilometer-Per-Liter(KPL)
+    """
+# This program show car's Kilometer-Per-Liter(KPL)
+def show_kpl():
+    
+    # This statement gets kilometer arived from user.
+    km = float(input('Please enter the kilometers traveled by the car:'))
+
+    # This statement gets liter used from user.
+    lit = float(input('Please enter the number of liters consumed by the car:'))
+
+    # This statements shwo car's Kilometer-per-Liter
+    kpl = km/lit
+    print ("Your car's Kilometer-Per-Liter is", (kpl))
+
+# Function call.
+show_kpl()
