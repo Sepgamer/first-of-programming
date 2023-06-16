e8a42e51f6d0bb5f46538aea42aff366dd3f39ca
Sepgamer <126633525+Sepgamer@users.noreply.github.com>
2023-06-16 12:50:26 +0330

    kilometer_per_liter_cal.py

--- kilometer_per_liter_cal.py
+++ kilometer_per_liter_cal.py
@@ -1,18 +1,18 @@
 """
     *   Developer   : Sepehr Hosseini
     *   Email       : Sepgamer87@Gmail.Com
-    *   Description : # This program show car's Kilometer-Per-Liter(KPL)
+    *   Description : # This program shows car's Kilometer-Per-Liter(KPL)
     """
 # This program show car's Kilometer-Per-Liter(KPL)
 def show_kpl():
     
     # This statement gets kilometer arived from user.
-    km = float(input('Please enter the kilometers traveled by the car:'))
+    km = float(input("Please enter the kilometers traveled by the car:"))
 
     # This statement gets liter used from user.
-    lit = float(input('Please enter the number of liters consumed by the car:'))
+    lit = float(input("Please enter the number of liters consumed by the car:"))
 
-    # This statements shwo car's Kilometer-per-Liter
+    # This statements show car's Kilometer-per-Liter
     kpl = km/lit
     print ("Your car's Kilometer-Per-Liter is", (kpl))
 
