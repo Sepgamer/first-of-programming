0735312903f494133010fe5c638438d491b47606
Sepgamer <126633525+Sepgamer@users.noreply.github.com>
2023-06-17 15:15:37 +0330

    calories _burned_calculator.py

--- calories _burned_calculator.py
+++ calories _burned_calculator.py
@@ -1,26 +1,26 @@
 """
     *   Developer   : Sepehr Hosseini
     *   Email       : Sepgamer87@Gmail.Com
-    *   Description : This program shows how much calories do you burn after 10, 15, 20, 25 and 30 mins running on special tredmill
+    *   Description : This program shows how much calories do you burn after 10, 15, 20, 25 and 30 mins running on special treadmill
     """
-# This program shows how much calories do you burn after 10, 15, 20, 25 and 30 mins running on special tredmill
+# This program shows how much calories do you burn after 10, 15, 20, 25 and 30 mins running on special treadmill
 
-# This statement is about how much calories you burn after 1 minutes on tredmill
+# This statement is about how much calories you burn after 1 minutes on treadmill
 base_cal = 4.2
 
-# This function calculates how much calories you burn after 10, 15, 20, 25 and 30 mins running on tredmill
+# This function calculates how much calories you burn after 10, 15, 20, 25 and 30 mins running on treadmill
 
-# This statement shows Respectively after 10, 15, 20, 25 and 30 minutes running on tredmill
+# This statement shows Respectively after 10, 15, 20, 25 and 30 minutes running on treadmill
 def time_run():
-    print("Respectively after 10, 15, 20, 25 and 30 minutes running on tredmill")
+    print("Respectively after 10, 15, 20, 25 and 30 minutes running on treadmill")
 
 def calories_burn():
     for time in [10, 15, 20, 25, 30]:
 
-        # This statement calculates calories burned after 10, 15, 20, 25 and 30 mins running on tredmill
+        # This statement calculates calories burned after 10, 15, 20, 25 and 30 mins running on treadmill
         burns = base_cal * time
 
-        # This statement shows calories burned after 10, 15, 20, 25 and 30 mins running on tredmill
+        # This statement shows calories burned after 10, 15, 20, 25 and 30 mins running on treadmill
         print("You burn", burns,"calories")
         print("-----------------------")
 
